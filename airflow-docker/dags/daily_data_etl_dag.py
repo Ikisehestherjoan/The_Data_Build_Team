6618ad611f510f5618e702878f6fcb2b63afe972
motunrayom <motunrayom.akinyele@gmail.com>
2025-04-21 08:55:14 +0100

    update the dag file

--- airflow-docker/dags/daily_data_etl_dag.py
+++ airflow-docker/dags/daily_data_etl_dag.py
@@ -3,9 +3,12 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 import subprocess
 
-def run_python_scripts():
-    # Assuming the python scripts are in the /app directory in the python container
+def run_python_extract():
+    
     subprocess.run(["docker", "exec", "pythonapp", "python", "/app/extract_rawdata.py"], check = True)
+
+def run_python_load():
+    
     subprocess.run(["docker", "exec", "pythonapp", "python", "/app/load_refine_data.py"], check=True)
 
 default_args = {
@@ -15,17 +18,30 @@ default_args = {
     'retries': 1,
 }
 
-dag = DAG(
+with DAG(
     'python_task_dag',
     default_args=default_args,
     description='A simple DAG to run Python scripts in the Python container',
     schedule_interval='0 8 * * *',  # Set to None for manual triggering or cron expression for scheduled runs
-)
+    catchup = False
+) as dag:
+    # Task to extract from api
+    run_python_extract = PythonOperator(
+        task_id='run_python_extract',
+        python_callable=run_python_extract,
+        dag=dag,
+    )
+    # Task to transform and load to postgres
+    run_python_load = PythonOperator(
+        task_id='run_python_load',
+        python_callable=run_python_load,
+        dag=dag,
+    )
 
-run_python_task = PythonOperator(
-    task_id='run_python_scripts',
-    python_callable=run_python_scripts,
-    dag=dag,
-)
 
-run_python_task
+    # Define task dependencies
+
+    run_python_extract >> run_python_load
+
+
+
